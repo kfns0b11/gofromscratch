d14b058412568cbd92202a9be0d89e04844fc201
kfngp <kfnsjc@gmail.com>
2021-10-07 18:07:03 +0800

    feat(design-pattern): add singleton design pattern
    Learn GOF design patterns, implementate singleton design pattern and
    three implementations in go: lazy instantial, eager instantial and
    elegant way.

---
+++ internal/designpattern/eagersingleton.go
@@ -0,0 +1,14 @@
+// singleton eager instantiation, means that global singleton
+// instance is created when the package is loaded. It will
+// cause longer to load if singleton type is time-consuming.
+
+package designpattern
+
+type singleton struct {
+}
+
+var ins *singleton = &singleton{}
+
+func GetInsOr() *singleton {
+	return ins
+}
---
+++ internal/designpattern/elegantsingleton.go
@@ -0,0 +1,19 @@
+// Implementate singleton design pattern elegantly in go with
+// the help of sync.Once
+
+package designpattern
+
+import "sync"
+
+type singleton struct {
+}
+
+var ins *singleton
+var once sync.Once
+
+func GetIns() *singleton {
+	once.Do(func() {
+		ins = &singleton{}
+	})
+	return ins
+}
